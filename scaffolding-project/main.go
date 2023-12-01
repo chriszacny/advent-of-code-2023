5d5e6a14898e5ba63593b143e57dd5e2fba1ff73
Chris Zacny <czacny@gmail.com>
2023-12-01 13:38:23 -0800

    chore: Add a scaffolding project

---
+++ scaffolding-project/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getMultiLineStringFromFile(file *os.File) string {
+	var b strings.Builder
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Fprintf(&b, "%s\n", scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(fmt.Sprintf("err: scanning file error\n"))
+	}
+	return b.String()
+}
+
+func main() {
+	filename := "in.dat"
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(fmt.Sprintf("err: error opening file %s\n", filename))
+	}
+	defer file.Close()
+
+	mls := getMultiLineStringFromFile(file)
+	fmt.Printf("%s\n", mls)
+}
